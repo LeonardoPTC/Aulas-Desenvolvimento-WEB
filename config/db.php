2817c7ce5898fa5dc2e71f74841a7764123a403d
LeonardoPTC <leonardopereiratc@gmail.com>
2025-08-07 20:42:27 -0300

    Primeira Aula 07_08_2025

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$dbname = 'aula';
+
+$conn = new mysqli($host, $user, $pass,$dbname);
+
+if($conn->connect_error){
+    die('Error na conexão: '. $conn->connect_error);
+}
+
+$conn->set_charset('utf8');
+
+?>
